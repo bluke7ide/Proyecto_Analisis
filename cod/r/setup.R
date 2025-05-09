13184614c4416224eb25a10d9c3c1759380a94a3
bluke7ide <luisfernandoamey@gmail.com>
2025-05-08 23:49:36 -0600

    feat: elimina el proyecto pasado

--- cod/r/setup.R
+++ cod/r/setup.R
@@ -17,8 +17,3 @@ pacman::p_load(
 
 source("cod/r/scrapW.R")
 source("cod/r/scrapT.R")
-
-# Módulos de Python -------------------------------------------------------
-
-# source_python("cod/python/setup.py")
-# source_python("cod/python/ProcesadorTexto.py")
